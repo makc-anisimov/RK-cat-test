3e99c7294a532b7904313ef3e91261b36df45b70
makc-anisimov <makc.anisimov@gmail.com>
2025-04-08 00:12:45 +0300

    add button, checkboxes

--- src/components/cats/cats.tsx
+++ src/components/cats/cats.tsx
@@ -1,10 +1,12 @@
-import styles from './cats.module.scss'
+import { Checkbox } from "../checkbox/checkbox";
+import styles from "./cats.module.scss";
 
 export const Cats = () => {
- return (
-  <div className={styles.cats}>
-
-  </div>
-
- )
-}
+  return (
+    <div className={styles.cats}>
+      <Checkbox />
+      <Checkbox />
+      <button className={styles.button} onClick={() => console.log("click")}>Get cat</button> 
+    </div>
+  );
+};
---
+++ src/components/checkbox/checkbox.tsx
@@ -0,0 +1,14 @@
+import styles from "./checkbox.module.scss";
+export const Checkbox = () => {
+  return (
+    <label className={styles.checkBoxContainer}>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        // checked={filterArchive}
+        // onChange={() => }
+      />
+      sdaasdasda
+    </label>
+  );
+};
